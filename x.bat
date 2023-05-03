391f12d0599f013bff692570d24a7eec55445f1b
lucruum <naoh432@mail.ru>
2023-05-03 13:57:09 +0300

    Поправил частичную очистку экрана x.bat'ом

--- x.bat
+++ x.bat
@@ -1,9 +1,12 @@
-set command= ^
-    cls ^& ^
-    black music.py --line-length=120 ^& ^
-    flake8 music.py --max-line-length=120 ^& ^
-    mypy music.py --pretty --show-error-context --strict ^& ^
-    python -m doctest music.py ^& ^
-    python music.py
+@echo off
+
+@REM Одиночный cls в Windows Terminal'е не всегда полностью очищает экран,
+@REM из-за чего вывод предыдущей команды наслаивается на вывод текущей
+set "command=          cls & cls &"
+set "command=%command% black music.py --line-length=120 &"
+set "command=%command% flake8 music.py --max-line-length=120 &"
+set "command=%command% mypy music.py --pretty --show-error-context --strict &"
+set "command=%command% python -m doctest music.py &"
+set "command=%command% python music.py"
 
 python -m on_touch music.py "%command%"
