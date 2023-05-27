f0c78c4ce14f10e2b4d8fe3c59b916f6fdbfd317
lucruum <naoh432@mail.ru>
2023-05-27 19:37:24 +0300

    Переход на poetry

--- x.bat
+++ x.bat
@@ -3,10 +3,10 @@
 @REM Одиночный cls в Windows Terminal'е не всегда полностью очищает экран,
 @REM из-за чего вывод предыдущей команды наслаивается на вывод текущей
 set "command=          cls & cls &"
-set "command=%command% black music.py --line-length=120 &"
-set "command=%command% flake8 music.py --max-line-length=120 &"
-set "command=%command% mypy music.py --pretty --show-error-context --strict &"
-set "command=%command% python -m doctest music.py &"
-set "command=%command% python music.py"
+set "command=%command% poetry run black music.py &"
+set "command=%command% poetry run flake8 music.py &"
+set "command=%command% poetry run mypy music.py &"
+set "command=%command% poetry run python -m doctest music.py &"
+set "command=%command% poetry run python music.py"
 
 python -m on_touch music.py "%command%"
